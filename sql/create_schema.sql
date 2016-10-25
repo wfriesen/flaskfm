c522736e7411dd2fe7108644d8eeaef52bc98781
William Friesen <wfriesen@gmail.com>
2016-10-25 18:37:19 +1100

    Added "Top 10 Artists Per Year" endpoint

--- sql/create_schema.sql
+++ sql/create_schema.sql
@@ -7,3 +7,32 @@ CREATE TABLE scrobble (
 );
 
 COPY scrobble(artist, album, track, scrobble_timestamp) FROM '/dataimport/scrobbles.csv' DELIMITER ',' CSV;
+
+CREATE VIEW artist_plays_per_year AS
+WITH artist_plays_per_year AS (
+  SELECT
+    CAST(TO_CHAR(s.scrobble_timestamp, 'YYYY') AS INTEGER) y
+  , artist
+  , COUNT(*) play_count
+  , ROW_NUMBER() OVER (
+      PARTITION BY CAST(TO_CHAR(s.scrobble_timestamp, 'YYYY') AS INTEGER)
+      ORDER BY COUNT(*) DESC
+    ) play_rank
+  FROM scrobble s
+  GROUP BY
+    y
+  , artist
+  ORDER BY
+    y DESC
+  , play_rank DESC
+)
+SELECT
+  y AS "year"
+, artist
+, play_count
+, play_rank
+FROM artist_plays_per_year
+WHERE play_rank <= 10
+ORDER BY
+  y DESC
+, play_rank;
--- web/app.py
+++ web/app.py
@@ -1,6 +1,8 @@
 #!flaskfm/bin/python
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
 from secrets import database_uri
 
 app = Flask(__name__)
@@ -28,6 +30,30 @@ def recent_scrobbles():
         'timestamp': scrobble.scrobble_timestamp
     } for scrobble in scrobbles]
     return jsonify({'scrobbles': scrobbles_json})
+
+
+@app.route(
+    '/flaskfm/api/v0.1/top_artists_per_year/<int:year>',
+    methods=['GET']
+)
+def top_artists_per_year(year):
+    s = text("""SELECT
+  year
+, artist
+, play_count
+, play_rank
+FROM artist_plays_per_year
+WHERE year = :year""")
+
+    results = db.engine.execute(s, year=year)
+
+    results_json = [{
+        'year': year,
+        'artist': artist,
+        'play_count': play_count,
+        'play_rank': play_rank
+    } for (year, artist, play_count, play_rank) in results]
+    return jsonify({'top_artists_per_year': results_json})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
