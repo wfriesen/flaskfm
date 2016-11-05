ce59c52eac56f388b36ff461e87a1614ad27503b
William Friesen <wfriesen@gmail.com>
2016-11-05 12:41:03 +1100

    Use environment file to pass secrets to containers

--- api/flaskfm/__init__.py
+++ api/flaskfm/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
-from secrets import database_uri
+import os
+
 from models import db
 
 app = Flask(__name__)
+
+pg_username = os.environ['POSTGRES_USER']
+pg_password = os.environ['POSTGRES_PASSWORD']
+pg_db = os.environ['POSTGRES_DB']
+database_uri = 'postgresql://%s:%s@db/%s' % (pg_username, pg_password, pg_db)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 db.init_app(app)
-
 
 from flaskfm import views
--- api/flaskfm/secrets.py
+++
@@ -1,6 +0,0 @@
-import os
-
-database_uri = os.environ.get(
-    'POSTGRES_URI',
-    'postgresql://docker:docker@db/docker'
-)
