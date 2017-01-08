850f18847ec06bc6c6851ee4a68a919f747c1f47
William Friesen <wfriesen@gmail.com>
2017-01-08 21:22:28 +1100

    Load pitch data, using table partitioning.
    Loading ~100m rows severely degraded performance.
    Increased timeout for starting api container, since the db will
    now take a while to start up first go around as it loads data

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A last.fm clone using [Flask](http://flask.pocoo.org/)
 - Use [Last.fm to csv](https://benjaminbenben.com/lastfm-to-csv/) to get a CSV of your (or any users) scrobbles and save it as `db/scrobbles.csv`.
 - Analyze some audio files that you have scrobbled. From within the `scripts` directory:
   - Build the docker image with `docker build -t audioscripts .`
-  - Run the actual processing with `docker run -v CODE_DIRECTORY/flaskfm/scripts/output:/volume/output -v MUSIC_DIRECTORY:/volume/files -it audioscripts`, replacing `CODE_DIRECTORY` with wherever you have cloned this repository, and `MUSIC_DIRECTORY` with the location of some (properly tagged) .mp3 and/or .flac files. These must be absolute paths.
+  - Run the actual processing with `docker run -v CODE_DIRECTORY/flaskfm/db/audio_data:/volume/output -v MUSIC_DIRECTORY:/volume/files -it audioscripts`, replacing `CODE_DIRECTORY` with wherever you have cloned this repository, and `MUSIC_DIRECTORY` with the location of some (properly tagged) .mp3 and/or .flac files. These must be absolute paths.
 - Run `docker-compose up`
 
 ## Thanks
--- db/sql/create_schema.sql
+++ db/sql/create_schema.sql
@@ -5,7 +5,67 @@ CREATE TABLE sample_data (
 , scrobble_timestamp TIMESTAMPTZ NOT NULL
 );
 
+CREATE TABLE files (
+  id INTEGER PRIMARY KEY NOT NULL
+, name TEXT NOT NULL
+, artist_name TEXT NOT NULL
+, album_name TEXT NOT NULL
+, track_name TEXT NOT NULL
+);
+
+CREATE TABLE onsets (
+  file_id INTEGER REFERENCES files(id)
+, onset NUMERIC
+);
+
+CREATE TABLE pitch (
+  file_id INTEGER REFERENCES files(id)
+, time NUMERIC
+, pitch NUMERIC
+);
+
 COPY sample_data(artist, album, track, scrobble_timestamp) FROM '/dataimport/scrobbles.csv' DELIMITER ',' CSV;
+
+COPY files(id, name, artist_name, album_name, track_name) FROM '/dataimport/files.csv' DELIMITER ',' CSV;
+
+COPY onsets(file_id, onset) FROM '/dataimport/onsets.csv' DELIMITER ',' CSV;
+
+DO $$DECLARE
+  file RECORD;
+  partition_name TEXT;
+BEGIN
+
+  FOR file IN (
+    SELECT
+      id
+    FROM files
+    GROUP BY
+      id
+  )
+  LOOP
+
+    partition_name := 'pitch_' || file.id;
+    EXECUTE 'CREATE TABLE ' || partition_name || ' (check (file_id = ' || file.id || ')) INHERITS (pitch);';
+
+  END LOOP;
+
+END
+$$;
+
+CREATE OR REPLACE FUNCTION pitch_insert()
+RETURNS TRIGGER AS $$
+BEGIN
+  EXECUTE 'INSERT INTO pitch_' || NEW.file_id || '(file_id, time, pitch) VALUES (' || NEW.file_id || ', ' || NEW.time || ', ' || NEW.pitch || ');';
+  RETURN NULL;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_pitch_insert
+  BEFORE INSERT ON pitch
+  FOR EACH ROW EXECUTE PROCEDURE pitch_insert();
+
+COPY pitch(file_id, time, pitch) FROM '/dataimport/pitch.csv' DELIMITER ',' CSV;
 
 CREATE OR REPLACE FUNCTION import_sample_data()
 RETURNS void AS $$
