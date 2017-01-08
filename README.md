46c42fbc6f16b8eb5a39e9f1d6d2349d6d6e1de3
William Friesen <wfriesen@gmail.com>
2017-01-08 16:03:51 +1100

    Added audio file processing
    Includes a docker file to run this script in a contianer, since it
    requires some specific packages to be installed, namely aubio-tools
    to perform the analysis.

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ A last.fm clone using [Flask](http://flask.pocoo.org/)
 
 ## Usage
 - Use [Last.fm to csv](https://benjaminbenben.com/lastfm-to-csv/) to get a CSV of your (or any users) scrobbles and save it as `db/scrobbles.csv`.
+- Analyze some audio files that you have scrobbled. From within the `scripts` directory:
+  - Build the docker image with `docker build -t audioscripts .`
+  - Run the actual processing with `docker run -v CODE_DIRECTORY/flaskfm/scripts/output:/volume/output -v MUSIC_DIRECTORY:/volume/files -it audioscripts`, replacing `CODE_DIRECTORY` with wherever you have cloned this repository, and `MUSIC_DIRECTORY` with the location of some (properly tagged) .mp3 and/or .flac files. These must be absolute paths.
 - Run `docker-compose up`
 
 ## Thanks
@@ -11,3 +14,4 @@ FlaskFM makes use of the following projects (among others):
 - [Flask](http://flask.pocoo.org/)
 - [Flask-SQLAlchemy](http://flask-sqlalchemy.pocoo.org/)
 - [wait-for-it](https://github.com/vishnubob/wait-for-it)
+- [aubio](https://aubio.org/)
---
+++ scripts/process.py
@@ -0,0 +1,53 @@
+import glob
+import taglib
+from subprocess import check_output
+
+AUDIO_PATH = '/volume/files/'
+OUTPUT_PATH = '/volume/output/'
+
+
+def get_onsets(audio_file):
+    for line in ''.join(check_output(['aubioonset', audio_file])).split('\n'):
+        if len(line) > 0:
+            yield line
+
+
+def get_pitches(audio_file):
+    for line in ''.join(check_output(['aubiopitch', audio_file])).split('\n'):
+        if len(line) > 0:
+            yield line.split(' ')
+
+
+def get_files():
+    return glob.glob(AUDIO_PATH + '**/*.flac') + \
+        glob.glob(AUDIO_PATH + '**/*.mp3')
+
+
+def write_onsets(id, filename):
+    with open(OUTPUT_PATH + 'onsets.csv', 'a') as o:
+        for onset in get_onsets(filename):
+            o.write(','.join([str(id), onset]) + '\n')
+
+
+def write_pitches(id, filename):
+    with open(OUTPUT_PATH + 'pitch.csv', 'a') as o:
+        for time, pitch in get_pitches(filename):
+            o.write(','.join([str(id), time, pitch]) + '\n')
+
+with open(OUTPUT_PATH + 'files.csv', 'w') as o:
+    for i, audio_file in enumerate(get_files(), 1):
+        print('Processing file ' + str(i) + ': ' + audio_file)
+        filename = audio_file[len(AUDIO_PATH):]
+        tags = taglib.File(audio_file).tags
+        values = [
+            '"' + tags[key][0].encode('utf-8') + '"' for key in [
+                'ARTIST',
+                'ALBUM',
+                'TITLE'
+            ]
+        ]
+        values = [str(i), '"' + filename + '"'] + values
+        csv_line = ','.join(values)
+        o.write(csv_line + '\n')
+        write_onsets(i, AUDIO_PATH + filename)
+        write_pitches(i, AUDIO_PATH + filename)
